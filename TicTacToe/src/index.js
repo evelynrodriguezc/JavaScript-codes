53cd7e350b19f733d96af60d025ee8f914c6ac29
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2022-10-21 17:26:24 -0500

    adding algorithm symbols x or o

---
+++ TicTacToe/src/index.js
@@ -0,0 +1,84 @@
+const board = document.querySelector("#board");
+const listOfSlots = [];
+let enemyMovements = [];
+let currentSlot;
+
+let buttons = document.querySelectorAll(".options--button");
+
+let player1 = {
+  active: false,
+  value: "",
+};
+
+let player2 = {
+  active: false,
+  value: "",
+};
+
+buttons = Array.from(buttons);
+
+board.classList.add("disabled");
+
+function printValue(event) {
+  const target = event.target;
+  currentSlot = target;
+  if (player1.active) {
+    target.innerText = player1.value;
+    player1.active = false;
+    player2.active = true;
+    myEnemy()
+  } else {
+    target.innerText = player2.value;
+    player2.active = false;
+    player1.active = true;
+  }
+
+  currentSlot.classList.add("disabled");
+  currentSlot.removeEventListener("click", printValue); // to not reescribe
+}
+
+function createSlots() {
+  for (let index = 0; index < 9; index++) {
+    const slot = document.createElement("div");
+
+    slot.addEventListener("click", printValue);
+
+    listOfSlots.push(slot);
+  }
+  enemyMovements = [...listOfSlots];
+  board.append(...listOfSlots);
+}
+
+function myEnemy() {
+    const enemySlot = enemyMovements[randomNumber(0, enemyMovements.length - 1)]
+    enemyMovements = enemyMovements.filter(elem => elem.id !== enemySlot.id);
+    enemySlot.click()
+}
+
+function randomNumber(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) );
+}
+
+createSlots();
+
+buttons.map((button) => {
+  button.addEventListener("click", (event) => {
+    const target = event.target;
+    const value = target.textContent;
+
+    board.classList.remove("disabled");
+
+    if (value === "⭕") {
+      player1.value = value;
+      player1.active = true;
+      player2.value = "❌";
+    } else {
+      player1.value = "⭕";
+      player2.value = "❌";
+    }
+
+    buttons.map((button) => {
+      button.disabled = true;
+    });
+  });
+});
