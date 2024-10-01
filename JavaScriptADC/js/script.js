f819c196095c9e6333a9dcd8222d68aa7d904add
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2024-10-01 17:58:28 -0500

    adding JavaScript datastructureexamples as sets, arrays and dictionaries

---
+++ JavaScriptADC/js/script.js
@@ -0,0 +1,34 @@
+
+function addTask(){
+    let taskInput = document.getElementById("new-task");
+    let taskText = taskInput.value.trim();
+
+    if(taskText === ""){
+        alert("Please add a task");
+        return;
+    }
+
+    let newTask = document.createElement("li");
+    newTask.innerHTML=`${taskText} <button onclick="deleteTask(this)">Delete</button>`; //?
+    newTask.addEventListener("click", function(){
+        newTask.classList.toggle("completed");
+    });
+
+    let listTasks = document.getElementById("task-list");
+    listTasks.appendChild(newTask);
+    taskInput.value="";
+}
+    document.getElementById("new-task").addEventListener("keydown", function(event){
+        if(event.key === "Enter"){
+            addTask();
+        }
+});
+
+
+
+function deleteTask(button){
+    let task = button.parentElement
+    task.remove()
+}
+
+
