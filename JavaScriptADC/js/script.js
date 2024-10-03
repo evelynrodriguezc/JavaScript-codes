ccb4980b601f105da8b3a74e0025d2b34ff38470
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2024-10-03 17:06:56 -0500

    adding dictionaries and sets basic operations

--- JavaScriptADC/js/script.js
+++ JavaScriptADC/js/script.js
@@ -1,4 +1,6 @@
 let number = [];
+let dictionary = {};
+let set = new Set();
 
 function addTask(){
     let taskInput = document.getElementById("new-task");
@@ -75,4 +77,104 @@ function unshiftElement(){
     }else{
         alert("Enter a valid value")
     }
+}
+
+//Dictionaries
+
+function addDictionary(){
+    let key = document.getElementById("keyInput").value.trim();
+    let value = document.getElementById("valueInputDic").value.trim();
+    if(key !== "" && value !== ""){
+        dictionary[key]=value;
+        document.getElementById("keyInput").value="";
+        document.getElementById("valueInputDic").value="";
+        updateResultDic();
+    }else{
+        alert("Please enter a key and a valid value");
+    }
+}
+
+function updateResultDic(){
+    let result = document.getElementById("resultDictionary");
+    let message = "Dictionary:<br>";
+    for(let key in dictionary){
+        message += `${key}: ${dictionary[key]}<br>`;
+    }
+    result.innerHTML = message;
+}
+
+function modifyDictionary(){
+    let key = document.getElementById("keyInput").value.trim();
+    let value = document.getElementById("valueInputDic").value.trim();
+    if(key in dictionary){
+        dictionary[key] = value;
+        document.getElementById("keyInput").value="";
+        document.getElementById("valueInputDic").value="";
+        updateResultDic();
+    }else{
+        alert("The key doesn't exist in the dictionary, so it can't be modified")
+    }
+}
+
+function deleteKey(){
+    let key = document.getElementById("keyInput").value.trim();
+    if(key in dictionary){
+        delete dictionary[key];
+        document.getElementById("keyInput").value="";
+        updateResultDic();
+    }else{
+        alert("The key doesn't exist in the dictionary, so it can't be deleted")
+    }
+}
+
+//Sets
+
+//valueInputSet
+//addSet()
+//modifySet()
+//deleteSetValue()
+//resultSet
+
+
+
+function addSet(){
+    let value = document.getElementById("valueInputSet").value.trim();
+    if(value !== ""){
+        set.add(value);
+        document.getElementById("valueInputSet").value="";
+        updateResultSet();
+    }else{
+        alert("Please enter a valid value");
+    }
+}
+
+function updateResultSet(){
+    let result = document.getElementById("resultSet");
+    result.innerHTML = `Set: [${Array.from(set).join(", ")}]`
+}
+
+function modifySet(){
+    let value = document.getElementById("valueInputSet").value.trim();
+    if(set.has(value)){
+        set.delete(value);
+        let newValue = prompt("Enter a new value to replace");
+        if(newValue && newValue.trim() !== ""){
+            set.add(newValue.trim())
+            updateResultSet();
+        }
+    }else{
+        alert("Please enter a valid value");
+    }
+    document.getElementById("valueInputSet").value = "";
+}
+
+function deleteSetValue(){
+    let value = document.getElementById("valueInputSet").value.trim();
+    if(set.has(value)){
+        set.delete(value);
+        document.getElementById("valueInputSet").value = "";
+        updateResultSet();
+        }else{
+            alert("Please enter a valid value")
+        }
 }
