3582319f3b78e11609cc379ab648e1bf723ed4a0
evelynrodriguezc <evelynrodriguezc0@gmail.com>
2024-10-10 16:21:02 -0500

    adding styles and icons

--- chatbot/js/script.js
+++ chatbot/js/script.js
@@ -3,11 +3,11 @@
 const chatBox = document.getElementById("chat-box");
 
 const answers = {
-    "hello " : "Hi! How're you doing?",
-    "what's your name?" : "I'm an interactive bot made with JavaScript.",
-    "bye" : "Goodbye, it was a pleasure.",
-    "what can you do?" : "I can answer basic quesionts and help you with whatever you might need.",
-    "default" : "Sorry, I don't understand that question, try again"
+    "hello" : " Hi! How're you doing?",
+    "what's your name?" : " I'm an interactive bot made with JavaScript.",
+    "bye": " Goodbye, it was a pleasure.",
+    "what can you do?" : " I can answer basic quesionts and help you with whatever you might need.",
+    "default" : " Sorry, I don't understand that question, try again"
 }
 
 function sendMessage(){
@@ -20,15 +20,17 @@ function sendMessage(){
     document.getElementById("user-input").value="";
 }
 
-function appendMessage(sender, message){
-    const messageElement = document.createElement("p");
-    messageElement.textContent = `${sender}:${message}`;
-    chatBox.appendChild(messageElement);
-    chatBox.scrollTop = chatBox.scrollHeight;
-}
 
 function checkEnter(event){
     if(event.key === "Enter"){
         sendMessage()
     }
+}
+
+function appendMessage(sender, message) {
+    const messageElement = document.createElement("p");
+    messageElement.classList.add(sender === "You" ? "user-message" : "bot-message");
+    messageElement.innerHTML = `<span class="icon">${sender === "You" ? "👤" : "🤖"}</span> ${sender}: ${message}`;
+    chatBox.appendChild(messageElement);
+    chatBox.scrollTop = chatBox.scrollHeight;
 }
